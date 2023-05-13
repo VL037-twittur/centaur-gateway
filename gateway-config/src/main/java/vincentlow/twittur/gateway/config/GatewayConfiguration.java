f1afcb231c1b79ce2af82fc23a83ad7da0eb9590
Vincent Low <vincent.low@gdn-commerce.com>
2023-05-14 01:25:18 +0700

    able to communicate gateway and services but can still escape from the security filter

--- gateway-config/src/main/java/vincentlow/twittur/gateway/config/GatewayConfiguration.java
+++ gateway-config/src/main/java/vincentlow/twittur/gateway/config/GatewayConfiguration.java
@@ -1,27 +1,25 @@
-//package vincentlow.twittur.gateway.config;
-//
-//import org.springframework.cloud.gateway.route.RouteLocator;
-//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class GatewayConfiguration {
-//
-//  @Bean
-//  public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-//
-//    return builder.routes()
-//        .route(p -> p
-//            .path("/auth/**")
-//            .uri("lb://cyclops-account-credential/api/v1/auth/**"))
-//        .route(p -> p
-//            .path("/accounts/**")
-//            .uri("lb://gargoyle-account-profile/api/v1/accounts/**"))
-//        // .route("cyclops-account-credential", r -> r.path("/api/v1/auth/**")
-//        // .uri("lb://cyclops-account-credential"))
-//        // .route("gargoyle-account-profile", r -> r.path("/api/v1/accounts/**")
-//        // .uri("lb://gargoyle-account-profile"))
-//        .build();
-//  }
-//}
+package vincentlow.twittur.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+  @Bean
+  public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+
+    return builder.routes()
+        .route("cyclops-account-credential", p -> p
+            .path("/auth/**")
+            .filters(f -> f.rewritePath("/auth/(?<path>.*)", "/api/v1/auth/${path}"))
+            .uri("lb://cyclops-account-credential"))
+        .route("gargoyle-account-profile", p -> p
+            .path("/accounts/**")
+            .filters(f -> f.rewritePath("/accounts/(?<path>.*)", "/api/v1/accounts/${path}"))
+            .uri("lb://gargoyle-account-profile"))
+        .build();
+  }
+}
